e794140e494724f7dabc6f321f4549f289469dd3
xterminal86 <xterminal86@gmail.com>
2026-07-14 12:54:56 +0300

    - WIP

--- src/main.cpp
+++ src/main.cpp
@@ -10,6 +10,8 @@
 
 #include <string_view>
 #include <iostream>
+#include <print>
+#include <ranges>
 
 using boost::asio::io_context;
 using boost::asio::co_spawn;
@@ -25,12 +27,131 @@ using boost::asio::ip::tcp;
 
 constexpr std::string_view delimiter = "\r\n\r\n";
 
+struct HttpRequest
+{
+  std::string Method;
+  std::string Path;
+  std::string Version;
+
+  std::unordered_map<std::string, std::string> Headers;
+
+  std::string Body;
+
+  std::string ToString()
+  {
+    std::stringstream ss;
+
+    ss << std::format("Method:  '{}'\n", Method)
+       << std::format("Path:    '{}'\n", Path)
+       << std::format("Version: '{}'\n", Version)
+       << "Headers:\n\n";
+
+    for (auto& kvp : Headers)
+    {
+      ss << std::format("'{}' = '{}'\n", kvp.first, kvp.second);
+    }
+
+    ss << "\n";
+    ss << "Body:\n";
+    ss << Body;
+
+    return ss.str();
+  }
+};
 
 awaitable<void> session(tcp::socket client_socket,
                         io_context& io_service)
 {
-  // code here
-  return awaitable<void>();
+  try
+  {
+    HttpRequest req;
+
+    std::string clientIp = client_socket.remote_endpoint().address().to_string();
+    std::println("{} connected", clientIp);
+
+    std::string rcv;
+
+    error_code ec;
+
+    // Read the HTTP request headers until we find the double CRLF
+    size_t bytes = co_await async_read_until(
+      client_socket,
+      dynamic_buffer(rcv),
+      delimiter,
+      redirect_error(use_awaitable, ec)
+    );
+
+    // Cannot use const here for some fucking reason.
+    std::string headersDelimiter = "\r\n";
+
+    std::vector<std::string> postData;
+
+    std::ranges::split_view<
+      std::ranges::ref_view<std::string>,
+      std::ranges::ref_view<std::string>
+    > splitted = std::views::split(rcv, headersDelimiter);
+
+    for (const auto& line : splitted)
+    {
+      std::string lineStr(line.begin(), line.end());
+      postData.push_back(lineStr);
+    }
+
+    for (size_t i = 0; i < postData.size(); i++)
+    {
+      if (i == 0)
+      {
+        auto spl = std::views::split(postData[i], ' ');
+        auto parts = spl | std::views::transform(
+          [](auto&& range)
+          {
+            return std::string(range.begin(), range.end());
+          }
+        ) | std::ranges::to<std::vector>();
+
+        if (parts.size() == 3)
+        {
+          req.Method  = parts[0];
+          req.Path    = parts[1];
+          req.Version = parts[2];
+        }
+        else
+        {
+          std::cerr << "Cannot parse METHOD PATH VERSION";
+        }
+      }
+      else
+      {
+      }
+    }
+
+    if (ec)
+    {
+      std::cerr << std::format("Error reading from client: '{}'", ec.message());
+      co_return;
+    }
+
+    size_t dp = rcv.find(delimiter);
+    std::println("*** {}", dp);
+
+    // Extract the request line (first line of the request)
+    std::string request_line =
+      rcv.substr(rcv.find(delimiter) + delimiter.length(), 3);
+    std::println("\nRequest: '{}'", request_line);
+
+    client_socket.shutdown(tcp::socket::shutdown_both, ec);
+    client_socket.close();
+
+    std::println("{} disconnected", clientIp);
+  }
+  catch (const std::exception& ex)
+  {
+    std::cerr << std::format(
+      "Exception caught during handling of client request: '{}'", ex.what()
+    );
+  }
+
+  co_return;
 }
 
 class Server
@@ -41,18 +162,37 @@ public:
     , acceptor_(io_service, tcp::endpoint(tcp::v4(), port))
     , socket_(io_service)
   {
+    std::println("Listening on port {}...", port);
     do_accept();
   }
 
 private:
   void do_accept()
   {
-    acceptor_.async_accept(
-      socket_,
-      [this](error_code ec)
+    std::println("Waiting for connections...");
+    co_spawn(
+      io_service_,
+      [this]() -> awaitable<void>
       {
-        // code here
-      }
+        while (true)
+        {
+          try
+          {
+            tcp::socket s = co_await acceptor_.async_accept(use_awaitable);
+            co_spawn(
+              io_service_,
+              session(std::move(s), io_service_), boost::asio::detached);
+          }
+          catch (const std::exception& ex)
+          {
+            std::cerr << std::format(
+              "Exception caught during accept: '{}'", ex.what()
+            );
+            break;
+          }
+        }
+      },
+      boost::asio::detached
     );
   }
 
@@ -67,18 +207,23 @@ int main(int argc, char* argv[])
   {
     if (argc != 2)
     {
-      std::cerr << "Usage: proxy_server";
-      std::cerr << " <listen_port>\n";
+      std::cerr << std::format("Usage: {} <PORT>\n", argv[0]);
+      return 1;
+    }
+
+    uint64_t port = std::stoull(argv[1]);
+    if (port > 65535)
+    {
+      std::cerr << "Port range must be [0 ; 65535]\n";
       return 1;
     }
 
     io_context io_service(1);
-    Server server(io_service, std::atoi(argv[1]));
+    Server server(io_service, port);
     io_service.run();
-
   }
   catch (const std::exception& e)
   {
-    std::cerr << "Exception: " << e.what() << std::endl;
+    std::cerr << std::format("Exception caught: '{}'\n", e.what());
   }
 }
